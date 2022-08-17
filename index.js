2a46eaa4c3499719f3fdcaf29521cd89f18ff078
xcixor <pndungu54@gmail.com>
2022-08-17 19:47:48 +0300

    fix(react): render comment correctly in v18

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 function Test() {
 	return <h1>Hello World!</h1>;
 }
 
-render(<Test />, document.getElementById("app"));
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
+
+root.render(<Test />);
