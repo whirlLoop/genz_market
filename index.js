1310c886ba6397c5121f6c2355583fcea57d980e
xcixor <pndungu54@gmail.com>
2022-09-06 07:56:01 +0300

    chore(components): add a hero component in the landing page

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function Test() {
-	return <h1>Hello World!</h1>;
-}
+import App from "./src/App";
 
 const rootElement = document.getElementById("app");
 const root = createRoot(rootElement);
 
-root.render(<Test />);
+root.render(<App />);
