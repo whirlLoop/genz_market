591d9262d83f3e12437ea78fc66b4a4ff5ddc4c2
xcixor <pndungu54@gmail.com>
2022-09-06 18:40:33 +0300

    style(navbar): improve the look of the navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Landing from "./pages/Landing";
 import "../public/style.css";
+import "../public/common.css";
 import Navbar from "./layouts/Navbar";
+import SignupLanding from "./pages/SignupLanding";
 
 export default function App() {
 	return (
 		<Box m="auto">
-			<Navbar />
+			<Box sx={{ borderBottom: 1, borderColor: "secondary.main", marginBottom:"10px", borderWidth:"0.01em" }}>
+				<Navbar />
+			</Box>
 			<Routes>
 				<Route path="/" element={<Landing />} />
+				<Route path="/signup/landing/" element={<SignupLanding />} />
 			</Routes>
 		</Box>
 	);
