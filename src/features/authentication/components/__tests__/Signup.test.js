f7288489686185d8b1d00d012a17a1c09211c4b9
xcixor <pndungu54@gmail.com>
2022-10-04 20:12:25 +0300

    test(Signup): add unit tests for signup page

--- src/features/authentication/components/__tests__/Signup.test.js
+++ src/features/authentication/components/__tests__/Signup.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Signup from "../Signup";
 
 describe("Signup", () => {
@@ -11,5 +11,74 @@ describe("Signup", () => {
 	});
 	it("renders correctly", () => {
 		expect(mountedSignupComponent).not.toBeNull();
+	});
+	it("toggles password on click", () => {
+		expect(screen.getByPlaceholderText("Password")).toHaveAttribute(
+			"type",
+			"password"
+		);
+		fireEvent.click(screen.getByLabelText("toggle password visibility"));
+		expect(screen.getByPlaceholderText("Password")).toHaveAttribute(
+			"type",
+			"text"
+		);
+	});
+});
+
+describe("Signup when user selects to join as a creator", () => {
+	let mountedSignupComponent;
+	beforeEach(() => {
+		mountedSignupComponent = render(
+			<Signup setUserType={jest.fn()} userType={"creator"} />
+		);
+	});
+	it("renders a message for creators", () => {
+		const { getByText } = mountedSignupComponent;
+		getByText("Start creating today!");
+	});
+	it("renders a button to switch registration to client", () => {
+		const { getByText } = mountedSignupComponent;
+		getByText("Or join as a client");
+	});
+	it("renders the basic creator fields", () => {
+		expect(
+			screen.getByRole("textbox", { name: /First Name/i })
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole("textbox", { name: /Last Name/i })
+		).toBeInTheDocument();
+		expect(screen.getByRole("textbox", { name: /Email/i })).toBeInTheDocument();
+		expect(
+			screen.getByRole("textbox", { name: /Phone Number/i })
+		).toBeInTheDocument();
+		expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+	});
+});
+
+describe("Signup when user selects to join as a client", () => {
+	let mountedSignupComponent;
+	beforeEach(() => {
+		mountedSignupComponent = render(
+			<Signup setUserType={jest.fn()} userType={"client"} />
+		);
+	});
+	it("renders a message for creators", () => {
+		const { getByText } = mountedSignupComponent;
+		getByText("Join to hire incredible talent today!");
+	});
+	it("renders a button to switch registration to client", () => {
+		const { getByText } = mountedSignupComponent;
+		getByText("Or join as a creator");
+	});
+	it("renders the basic client fields", () => {
+		expect(
+			screen.getByRole("textbox", { name: /First Name/i })
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole("textbox", { name: /Last Name/i })
+		).toBeInTheDocument();
+		expect(screen.getByRole("textbox", { name: /Email/i })).toBeInTheDocument();
+		expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+		expect(screen.getByPlaceholderText("Phone Number")).toBeInTheDocument();
 	});
 });
